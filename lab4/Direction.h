468908408224cbd78a427ae10555967a95bf235a
emilknievel <emil.vagstedt@gmail.com>
2015-02-15 19:13:24 +0100

    Deklarerat operator<< för Direction

--- lab4/Direction.h
+++ lab4/Direction.h
@@ -1,11 +1,14 @@
 /*
  * direction.h
  */
- 
+
  // 1.
 #ifndef DIRECTION_H
 #define DIRECTION_H
 
 enum Direction { Up, Down, Left, Right };
+
+// 2.
+std::ostream& operator<<(std::ostream& os, const Direction& dir);
 
 #endif */ DIRECTION_H */
